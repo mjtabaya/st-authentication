1988e644dbff83d06e9a9de652f559455e7380ae
mjtabaya <mjtabaya@gmail.com>
2019-07-28 21:31:13 +0800

    Add view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+  root 'static_pages#home'
+  get '/home', to: 'static_pages#home'
+  get '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
 end
